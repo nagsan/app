78fa4e92e69b033b7251921e59d8ded67d3e03ea
sanjana456 <sanjanakumari.c@gmail.com>
2018-09-05 16:16:20 +0530

    new modify

--- src/app/Employee.java
+++ src/app/Employee.java
@@ -1,5 +1,22 @@
 package app;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class Employee {
    private int id;
+   String f;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getF() {
+	return f;
+}
+public void setF(String f) {
+	this.f = f;
+}
+   
+   
 }
